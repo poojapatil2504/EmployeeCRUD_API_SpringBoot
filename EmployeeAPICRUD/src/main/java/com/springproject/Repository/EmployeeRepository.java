1ce3340250ed65980f2b43b42822963b3dac8b91
poojapatil2504 <146211091+poojapatil2504@users.noreply.github.com>
2024-12-31 16:12:37 +0530

    Add files via upload

---
+++ EmployeeAPICRUD/src/main/java/com/springproject/Repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.springproject.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.springproject.model.Employee;
+
+public interface EmployeeRepository  extends JpaRepository<Employee, Long>{
+	
+
+}
---
+++ EmployeeAPICRUD/src/main/java/com/springproject/controller/EmployeeController.java
@@ -0,0 +1,85 @@
+package com.springproject.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+
+import com.springproject.Repository.EmployeeRepository;
+import com.springproject.model.Employee;
+
+@RestController
+@RequestMapping("/api")
+public class EmployeeController {
+	
+	@Autowired
+	EmployeeRepository employeeRepository;
+	
+	@PostMapping("/employees")
+	public String createNewEmployee(@RequestBody Employee employ) {
+		
+		employeeRepository.save(employ);
+		
+		return "Employee created in database";
+	}
+	
+	@GetMapping("/employees")
+	public ResponseEntity<List<Employee>> getAllEmployees(){
+		List<Employee> empList = new ArrayList<>();
+		employeeRepository.findAll().forEach(empList :: add);
+		return new ResponseEntity<List<Employee>>(empList, HttpStatus.OK);
+	}
+	
+	@GetMapping("/employees/{empid}")
+	public ResponseEntity<Employee> getEmployeeById(@PathVariable long empid){
+		Optional<Employee> emp = employeeRepository.findById(empid);
+		if(emp.isPresent()) {
+			return new ResponseEntity<Employee>(emp.get(), HttpStatus.FOUND);
+		}else {
+			return new ResponseEntity<Employee>(HttpStatus.NOT_FOUND);
+		}
+	}
+	
+	@PutMapping("/employees/{empid}")
+	public String updateEmployeeById(@PathVariable long empid, @RequestBody Employee employee ) {
+		Optional<Employee> emp = employeeRepository.findById(empid);
+		if(emp.isPresent()) {
+			Employee existEmp = emp.get();
+			existEmp.setEmp_age(employee.getEmp_age());
+			existEmp.setEmp_city(employee.getEmp_city());
+			existEmp.setEmp_salary(employee.getEmp_salary());
+			existEmp.setEmp_name(employee.getEmp_name());
+			
+			employeeRepository.save(existEmp);
+			return "Employee details against Id "+empid +"updated";
+		}else {
+			return "Employee details does not exist for the empid" +empid;
+		}
+	}
+	
+	@DeleteMapping("/employees/{empid}")
+	public String deleteEmployeeByEmpId(@PathVariable long empid) {
+		employeeRepository.deleteById(empid);
+		return "Employee deleted successfully";
+	}
+	
+	@DeleteMapping("/employees")
+	public String deleteAllEmployees() {
+		employeeRepository.deleteAll();
+		return "Employee deleted successfully";
+				 }
+
+         }
